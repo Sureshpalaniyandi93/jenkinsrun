c636c70ec17139ba49aafd63bc44d348d1e792ae
Sureshpalaniyandi93 <141026951+Sureshpalaniyandi93@users.noreply.github.com>
2024-02-29 22:25:31 +0530

    Update sampleFile.java

--- jenkinsRunProject/src/test/java/sampleFile.java
+++ jenkinsRunProject/src/test/java/sampleFile.java
@@ -4,8 +4,35 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class sampleFile {
     public static void main(String[] args) {
-        WebDriverManager.chromedriver().setup();
+      WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        driver.get("https://uat.portal.infinitbar.com/");
+//        driver.get("https://uat.portal.infinitbar.com/");
+//        System.out.println("finally executed");
+//        driver.quit();
+        driver.get("https://www.google.com");
+
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("Samsung Galaxy");
+        searchBox.submit();
+
+        // Iterate through 10 pages of search results
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Page " + i + ":\n-------------------");
+
+            // Print text from the current page
+            System.out.println(driver.findElement(By.tagName("body")).getText());
+
+            // Click on the next page button
+          
+            // Wait for a short while for the next page to load
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Close the browser
+        driver.quit();
     }
 }
