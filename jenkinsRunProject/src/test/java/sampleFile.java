c85b56e5d8bf6475bc070dcbe52013f3a06d52a4
Sureshpalaniyandi93 <141026951+Sureshpalaniyandi93@users.noreply.github.com>
2024-02-29 23:02:04 +0530

    Update sampleFile.java

--- jenkinsRunProject/src/test/java/sampleFile.java
+++ jenkinsRunProject/src/test/java/sampleFile.java
@@ -1,6 +1,9 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 
 public class sampleFile {
     public static void main(String[] args) {
