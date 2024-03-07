b201609599fd4f115e80c7975f00420a5678fd74
Sureshpalaniyandi93 <141026951+Sureshpalaniyandi93@users.noreply.github.com>
2024-03-07 09:12:15 +0530

    Update sampleFile.java

--- jenkinsRunProject/src/test/java/sampleFile.java
+++ jenkinsRunProject/src/test/java/sampleFile.java
@@ -5,49 +5,51 @@ import org.testng.annotations.Test;
 
 
 public class sampleFile {
-   @Test(invocationCount = 1,priority = -1)
-    public void sample() {
+    public static void main(String[] args) {
 
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://uat.portal.infinitbar.com/");
-        String source = driver.getPageSource();
-        System.out.println(source);
-        driver.quit();
 
-    } @Test(invocationCount =2,priority = 5)
-    public void sample2() {
 
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com/");
-        String source = driver.getPageSource();
-        System.out.println(source);
-        driver.quit();
 
-    } @Test(invocationCount =3,priority = 0)
-    public void sample1() {
 
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://felix.keka.com/#/home/dashboard");
-        String source = driver.getPageSource();
-        System.out.println(source);
-        driver.quit();
+            WebDriverManager.chromedriver().setup();
+            WebDriver driver = new ChromeDriver();
+            driver.get("https://uat.portal.infinitbar.com/");
+            String source = driver.getPageSource();
+            System.out.println(source);
+//            driver.quit();
+
+
+
+//        public void sample2 () {
+//
+//            WebDriverManager.chromedriver().setup();
+//            WebDriver driver = new ChromeDriver();
+            driver.get("https://www.google.com/");
+            //String source = driver.getPageSource();
+//            System.out.println(source);
+//            driver.quit();
+
+//        }
+
+//        public void sample1 () {
+//
+//            WebDriverManager.chromedriver().setup();
+//            WebDriver driver = new ChromeDriver();
+            driver.get("https://felix.keka.com/#/home/dashboard");
+//            String source = driver.getPageSource();
+//            System.out.println(source);
+//            driver.quit();
+//        }
+
+
+//        public void sample12 () {
+//
+//            WebDriverManager.chromedriver().setup();
+//            WebDriver driver = new ChromeDriver();
+            driver.get("https://felix.keka.com/#/home/dashboard");
+//            String source = driver.getPageSource();
+//            System.out.println(source);
+            driver.quit();
+        }
     }
 
-
-
-
-
-    @Test(invocationCount =3,priority = 0)
-    public void sample12() {
-
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://felix.keka.com/#/home/dashboard");
-        String source = driver.getPageSource();
-        System.out.println(source);
-        driver.quit();
-    }
-}
